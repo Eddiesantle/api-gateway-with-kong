a08325ceb1963674cae5547ecd967028319d2273
Eddiesantle <wedleys8@gmail.com>
2024-06-16 16:52:05 -0300

    FEAT: doc details

--- node-api/api.js
+++ node-api/api.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// let counter = 0;
+let counter = 0;
 
 app.get("/", (req, res) => {
-    //    counter++;
-    //    console.log(`Request number ${counter}`);
+    counter++;
+    console.log(`APINODE:${port} - Request number: ${counter}`);
     res.send("Hello World!");
 });
 
